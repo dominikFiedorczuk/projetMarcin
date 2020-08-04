bb6d8b1a14c5d1a68a8d54d64711ab178e1d9a50
dominikFiedorczuk <dominik0076@op.pl>
2020-08-04 12:26:15 +0200

    Mise en place de connexion admin + création BDD

--- src/Controller/adminController.php
+++ src/Controller/adminController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Role;
+use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class adminController extends AbstractController {
     
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin/login", name="admin_login")
      */
-    public function admin(){
+    public function adminLogin(AuthenticationUtils $utils){
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+
         return $this->render(
-            'base.html.twig'
+            'admin/login.html.twig',
+            [
+                'error' => $error !== null,
+                'username' => $username
+            ]
+        );
+    }
+
+    /**
+     * @Route("logout", name="logout")
+     */
+    public function logout(){
+        
+    }
+
+    /**
+     * @Route("admin", name="admin_dashboard")
+     */
+    public function adminDashboard(){
+        return $this->render(
+            'admin/base.html.twig'
         );
     }
 }
---
+++ src/Entity/Images.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ImagesRepository::class)
+ */
+class Images
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Folders::class, inversedBy="images")
+     */
+    private $folder;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFolder(): ?Folders
+    {
+        return $this->folder;
+    }
+
+    public function setFolder(?Folders $folder): self
+    {
+        $this->folder = $folder;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
